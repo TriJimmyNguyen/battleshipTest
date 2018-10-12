c7669cbc1f5cebeac9f525dac6fe762492134b1c
Tri Jimmy Nguyen <tjnguyen.school@gmail.com>
2018-10-11 23:00:37 -0400

    classe PartieNavire implementer

--- src/modele/PartieNavire.java
+++ src/modele/PartieNavire.java
@@ -1,5 +1,23 @@
 package modele;
 
 public class PartieNavire {
-
+	private Position positionPartieNavire;
+	private boolean estFonctionnel;
+	
+	public PartieNavire(int x, int y) {
+		this.positionPartieNavire = new Position(x, y);
+		this.estFonctionnel = true;
+	}
+	
+	public Position getPositionPartieNavire() {
+		return this.positionPartieNavire;
+	}
+	
+	public boolean isFonctionnel() {
+		return this.estFonctionnel;
+	}
+	
+	public void setFonctionnelFalse() {
+		this.estFonctionnel = false;
+	}
 }
